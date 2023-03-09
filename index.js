8c07e8fd2238cd59b88fa71f4aed38b50f74666e
Tomas Levy <tomaslevy1999@gmail.com>
2023-03-08 23:22:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function changeColor(){
     let saludo = document.querySelector(".saludo");
     let chamuyo = document.querySelector(".chamuyo");
+    let chamuyo2 = document.querySelector(".chamuyo2");
     if(saludo.style.display=="none"){
         chamuyo.style.display="none";
+        chamuyo2.style.display="none";
         saludo.style.display="block"
     } else{
         saludo.style.display="none";
-        chamuyo.style.display="block"
+        chamuyo.style.display="block";
+        chamuyo2.style.display="block";
         console.log('hola')
     }
     
